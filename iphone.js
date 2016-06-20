f0c2c0bd7c9a79e849e7e1ca8d405532c4e4815b
AlvieHuang <Alvinh7@uci.edu>
2016-06-20 15:00:33 -0700

    18

--- iphone.js
+++ iphone.js
@@ -17,13 +17,13 @@ $document.read(function(){loadpage(startUrl);});
 function loadPage(url) {
   $('body').append('<div id="progress">Loading...</div>')
   scrollTo(0,0);
-  if (url=startUrl) {
+  if (url == startUrl) {
     var element = ' #header ul';
   } else{
     var element = ' #content';
   }
   $('#container').load(url + element, function(){
-    var title=$('h2').html() || 'Hello!';
+    var title = $('h2').html() || 'Hello!';
     $('h1').html(title);
     $('h2').remove();
     $('.leftButton').remove();
@@ -42,8 +42,9 @@ function loadPage(url) {
         e.preventDefault();
         loadPage(url);
       }
-    })
-  })
+    });
+    $('#progress').remove();
+  });
 }
 
 /*

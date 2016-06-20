0c187f7814c32b52183c3b6c0de97d1a4e0bf72c
AlvieHuang <Alvinh7@uci.edu>
2016-06-20 14:42:04 -0700

    15

--- iphone.js
+++ iphone.js
@@ -10,22 +10,54 @@ if (window.innerWidth && window.innerWidth <=480) {
   }
 }
 */
-
-$document.read(function(){loadpage();});
+var hist = [];
+var startUrl = 'index.html'
+$document.read(function(){loadpage(startUrl);});
 
 function loadPage(url) {
   $('body').append('<div id="progress">Loading...</div>')
-  if (url=undefined) {
-    $('#container').load('index.html #header ul', hijackLinks);
+  scrollTo(0,0);
+  if (url=startUrl) {
+    var element = ' #header ul';
   } else{
-    $('#contaiiner').load(url + ' #content', hijackLinks);
+    var element = ' #content';
   }
+  $('#container').load(url + element, function(){
+    var title=$('h2').html() || 'Hello!';
+    $('h1').html(title);
+    $('h2').remove();
+    $('.leftButton').remove();
+    hist.unshift({'url':url, 'title':title});
+    if (hist.length > 1) {
+      $('#header').append('<div class="leftButton">'+hist[1].title+'</div');
+      $('#header .leftButton').click(function(){
+        var thisPage = hist.shift();
+        var previousPage = hist.shift();
+        loadPage(previousPage.url);
+      });
+    }
+    $('#container a').click(function(e){
+      var url = e.target.href;
+      if (url.match(/AlvieHuang.github.io/)) {
+        e.preventDefault();
+        loadPage(url);
+      }
+    })
+  })
 }
 
+/*
 function hijackLinks() {
   $('#container a').click(function(e){
-    e.preventDefault();
-    loadPage(e.target.href);
+    var url = e.target.href;
+    if (url.match(AlvieHuang.github.io)){
+      e.preventDefault();
+      loadPage(e.target.href);
+    }
   });
+  var title=$('h2').html() || 'Hello!';
+  $('h1').html(title);
+  $('h2').remove();
   $('#progress').remove();
 }
+*/

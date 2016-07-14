edb5aa24d782df11f46b3982144e6dadd06cdac5
AlvieHuang <Alvinh7@uci.edu>
2016-07-13 17:56:46 -0700

    32

--- desktop.js
+++ desktop.js
@@ -1,12 +1,36 @@
 
 
+//
+//
 
 
 $(document).ready(function(){
-    $("#hide").mouseenter(function(){
-        $("p").hide();
-    });
-    $("#show").mouseenter(function(){
-        $("p").show();
-    });
-})
+    $( "#content p" ).hover(
+      function() {
+        $( this ).text("HOW DARE YOU HOVOR !!");
+      }, function() {
+        $( this ).text("this is a test");
+      }
+    );
+});
+
+$(document).ready(function(){
+    $( "#games #WvA" ).hover(
+      function() {
+        $(this).children("div").slideToggle("fast");
+      }, function() {
+        $(this).children("div").slideToggle("fast");
+      }
+    );
+});
+
+
+$(document).ready(function(){
+    $( "#games #Asteroid" ).hover(
+      function() {
+        $(this).children("div").fadeIn("fast");
+      }, function() {
+        $(this).children("div").fadeOut("fast");
+      }
+    );
+});

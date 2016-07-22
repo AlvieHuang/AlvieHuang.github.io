0b16079b70755e6dfb6605e3b4d9fde3b30e7feb
AlvieHuang <Alvinh7@uci.edu>
2016-07-22 16:45:21 -0700

    40

--- desktop.js
+++ desktop.js
@@ -3,46 +3,10 @@ var animation = true;
 
 //
 //
-var animateDown = function(targetElement, speed){
-    if (stop){return;}
-    $(targetElement).animate(
-        {
-            top:'100px'
-        },
-        {
-        duration: speed,
-        complete: function(){
-            animateUp(this, speed);
-            }
-        }
-    );
-};
 
-var animateUp = function(targetElement, speed){
-    $(targetElement).animate(
-        {
-            top:'10px'
-        },
-        {
-        duration: speed,
-        complete: function(){
-            animateDown(this, speed);
-            }
-        }
-    );
-};
-
-var animations = function(){
-  if (animation){
-    animation=false;
-    $("#animations span").text="unlock all animations";
-  }
-  else {
-    animation=true;
-    $("#animations span").text="lock all animations";
-  }
-};
-
+//************************************************************************
+//test
+//************************************************************************
 $(document).ready(function(){
     $( "#content p" ).hover(
       function() {
@@ -53,6 +17,25 @@ $(document).ready(function(){
     );
 });
 
+
+//************************************************************************
+//Function to lock animations
+//************************************************************************
+var animations = function(){
+  if (animation){
+    animation=false;
+    $("#games button").text("Unlock all animations");
+  }
+  else {
+    animation=true;
+    $("#games button").text("Lock all animations");
+  }
+};
+
+
+//************************************************************************
+//Section code for Witches Vs. Aliens
+//************************************************************************
 $(document).ready(function(){
     $( "#games #WvA" ).hover(
       function() {
@@ -60,8 +43,8 @@ $(document).ready(function(){
         if (animation){
           stop=false;
           $("#ball").fadeIn("fast");
-          animateDown($("#WvA .leftimg"),"slow");
-          animateDown($("#WvA .rightimg"),"slow");
+          animateWitch();
+          animateAlien();
         }
       },
       function() {
@@ -70,13 +53,81 @@ $(document).ready(function(){
           stop=true;
           $("#WvA .leftimg").finish();
           $("#WvA .rightimg").finish();
-          $("#ball").stop().fadeOut("fast");
+          $("#ball").finish().fadeOut("fast");
         }
       }
     );
 });
 
+//************************************************************************
+//All of the animation for Witches vs. Aliens
+//************************************************************************
+var animateWitch = function(){
+    if (stop){return;}
+    var targetElement = $("#witch");
+    targetElement.animate({top:'30%'},600,function()
+        {document.getElementById("witch").src = "images/witch-right.gif";}
+    );
+    targetElement.delay(100);
+    targetElement.animate({left:'30%'},900);
+    targetElement.delay(100);
+    targetElement.animate({left:'27%'},100,'linear');
+    targetElement.animate({left:'29%'},100,'linear',function()
+        {document.getElementById("witch").src = "images/witch-left.gif";}
+    );
+    targetElement.delay(100);
+    targetElement.animate({top:'27%', left:'15%'},300,'linear');
+    targetElement.animate({top:'20%', left:'7%'},200,'linear',function()
+        {document.getElementById("witch").src = "images/witch-back.gif";}
+    );
+    targetElement.animate({top:'10px', left:'3%'},600,function()
+        {
+          animateWitch();
+          document.getElementById("witch").src = "images/witch-front.gif";
+        }
+    );
+    targetElement.delay(300);
+};
 
+var animateAlien = function(){
+    if (stop){return;}
+    var targetElement = $("#alien");
+    targetElement.animate({top:'30%'},600,function()
+          {document.getElementById("alien").src = "images/alien-left.gif";}
+    );
+    targetElement.animate({right:'21%'},400,'linear',function()
+          {animateBall();}
+    );
+    targetElement.animate({right:'18%'},100,'linear');
+    targetElement.delay(100);
+    targetElement.animate({right:'23%'},300,'linear',function()
+          {document.getElementById("alien").src = "images/alien-back.gif";}
+    );
+    targetElement.animate({top:'26%', right:'25%'},100,'linear');
+    targetElement.animate({top:'23%'},100,'linear');
+    targetElement.animate({top:'18%', right:'23%'},100,'linear',function()
+          {document.getElementById("alien").src = "images/alien-right.gif";}
+    );
+    targetElement.animate({top:'10px', right:'3%'},600,function()
+          {
+            animateAlien();
+            document.getElementById("alien").src = "images/alien-front.gif";
+          }
+    );
+    targetElement.delay(1000);
+};
+
+var animateBall = function(){
+    var targetElement = $('#ball');
+    targetElement.animate({left:'-=21%'},600,'linear');
+    targetElement.delay(100);
+    targetElement.animate({left:'+=21%'},600,'linear');
+}
+
+
+//************************************************************************
+//Section code for Asteroid 9000
+//************************************************************************
 $(document).ready(function(){
     $( "#games #Asteroid" ).hover(
       function() {
